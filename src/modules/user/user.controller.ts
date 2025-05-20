9e269953f9723f1cdcc693b71a0464513183ce10
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-20 21:38:33 +0330

    feat(user): Implement user retrieval and creation functionality with TypeORM integration

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  findById(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findById(id);
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { User } from '@entities/user.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async findById(id: number) {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) throw new NotFoundException();
+    return user;
+  }
+
+  async create(createDto: CreateUserDto) {
+    const { mobile, fullname, amount } = createDto;
+    let user = await this.userRepository.findOneBy({ mobile });
+    if (!user) {
+      user = this.userRepository.create({
+        mobile,
+        fullname,
+        balance: amount,
+      });
+      await this.userRepository.save(user);
+    }
+    return user;
+  }
+}
